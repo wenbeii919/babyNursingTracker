77724249d04ecc7175c3807d85374897719559ae
helloworld101 <wbzamerica@gmail.com>
2020-08-05 13:56:26 -0700

    saving user instance to mongodb atlas users collection

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 // accessToken - a callback function, to take the user information to save to the db
 // refreshToken - to refresh the access token when it's out of effect
@@ -11,9 +14,7 @@ passport.use(
         callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile:', profile);
+        new User({ googleID: profile.id }).save();
     }
     )
 );
