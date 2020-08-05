93c960eaea773bd8d11f857ff4a67a8f9ec41922
helloworld101 <wbzamerica@gmail.com>
2020-08-05 14:44:28 -0700

    enables passport to use cookie for authentication

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,20 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+// user.id(internal db id) != profile.id(google oauth)
+// serializeUser takes a user model instance, and turns it into an id
+// so that deserializeUser can take that id to search/query in the users collection db
+// when the specific user is found, we return back the user
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
 // accessToken - a callback function, to take the user information to save to the db
 // refreshToken - to refresh the access token when it's out of effect
 passport.use(
@@ -14,14 +28,16 @@ passport.use(
         callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleID: profile.id })
-            .then((existingUser) => {
+        User.findOne({ googleID: profile.id }).then((existingUser) => {
                 if (existingUser) {
-
+                    // the given profile ID is existed in the collection already
+                    done(null, existingUser);
                 } else {
-                    new User({ googleID: profile.id }).save();
+                    new User({ googleID: profile.id })
+                    .save()
+                    .then(user => done(null, user));
                 }
-            })
-    }
+            });
+        }
     )
 );
