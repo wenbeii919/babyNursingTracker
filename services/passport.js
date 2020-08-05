423ac767ae6b9cec79fd3f97522c4926340e0ae8
helloworld101 <wbzamerica@gmail.com>
2020-08-05 14:03:34 -0700

    avoiding repeated users documents creation

--- services/passport.js
+++ services/passport.js
@@ -14,7 +14,14 @@ passport.use(
         callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        new User({ googleID: profile.id }).save();
+        User.findOne({ googleID: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+
+                } else {
+                    new User({ googleID: profile.id }).save();
+                }
+            })
     }
     )
 );
