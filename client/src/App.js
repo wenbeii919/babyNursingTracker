ff1b134202ab02323050e55a21b3f39c474803e0
helloworld101 <wbzamerica@gmail.com>
2020-08-06 13:48:14 -0700

    added CRA proxy

--- client/src/App.js
+++ client/src/App.js
@@ -11,12 +11,9 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/auth/google"
         >
-          Learn React
+          Sign in with google
         </a>
       </header>
     </div>
