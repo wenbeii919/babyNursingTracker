2e2516bcc6dddf7cc531bb16759c34641d68ee01
helloworld101 <wbzamerica@gmail.com>
2020-08-07 09:48:48 -0700

    action creator for fetchUser

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const dashboard = () => <h2>dashboard</h2>
@@ -7,19 +9,24 @@ const nursingNew = () => <h2>adding a new nursing item</h2>
 const landing = () => <h2>landing</h2>
 
 
-const App = () => {
-    return (
-        <div className="container">
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={landing} />
-                    <Route exact path="/nursings" component={dashboard} />
-                    <Route path="/nursings/new" component={nursingNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+    render() {
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={landing} />
+                        <Route exact path="/nursings" component={dashboard} />
+                        <Route path="/nursings/new" component={nursingNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    };
 };
 
-export default App;
+export default connect(null, actions)(App);
