4786ab9271b12520970c691bd01366b71ebd2c91
helloworld101 <wbzamerica@gmail.com>
2020-08-06 22:05:07 -0700

    initial react router config

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>
+const dashboard = () => <h2>dashboard</h2>
+const nursingNew = () => <h2>adding a new nursing item</h2>
+const landing = () => <h2>landing</h2>
+
 
 const App = () => {
     return (
         <div>
-            Hi 
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={landing} />
+                    <Route exact path="/nursings" component={dashboard} />
+                    <Route path="/nursings/new" component={nursingNew} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
