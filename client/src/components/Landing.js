b7d496abd1246a89a0479f6a1f48cf0bd8796fe4
helloworld101 <wbzamerica@gmail.com>
2020-08-07 14:41:10 -0700

    added client side app

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,11 +3,10 @@ import React from 'react';
 const Landing = () => {
     return (
         <div style={{ textAlign: 'center'}}>
-            {/* <img src="../landingPrince.jpg" alt="" class="circle responsive-img" /> */}
-                <h1>
-                    Mamadada!
-                </h1>
-                help you know your baby better!
+            <h1>
+                Mamadada!
+            </h1>
+            help you know your baby better!
         </div>
     );
 };
