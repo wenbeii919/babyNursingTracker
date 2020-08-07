fa98d65fd0776d55aa85f7638571b1fa59f43fe9
helloworld101 <wbzamerica@gmail.com>
2020-08-06 21:34:09 -0700

    redux setup

--- services/passport.js
+++ services/passport.js
@@ -28,17 +28,14 @@ passport.use(
         callbackURL: '/auth/google/callback',
         proxy: true
     }, 
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleID: profile.id }).then((existingUser) => {
-                if (existingUser) {
-                    // the given profile ID is existed in the collection already
-                    done(null, existingUser);
-                } else {
-                    new User({ googleID: profile.id })
-                    .save()
-                    .then(user => done(null, user));
-                }
-            });
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleID: profile.id })
+            if (existingUser) {
+                // when the given profile ID is existed in the collection already
+                return done(null, existingUser);
+            }
+            const user = await new User({ googleID: profile.id }).save();
+            done(null, user);
         }
     )
 );
