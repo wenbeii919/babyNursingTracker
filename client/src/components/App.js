a0117481d7ebac9c5f88d54cf7e4a2115504717a
helloworld101 <wbzamerica@gmail.com>
2020-08-06 23:53:40 -0700

    formatting header by using materialize css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => <h2>Header</h2>
+import Header from './Header';
 const dashboard = () => <h2>dashboard</h2>
 const nursingNew = () => <h2>adding a new nursing item</h2>
 const landing = () => <h2>landing</h2>
@@ -9,7 +9,7 @@ const landing = () => <h2>landing</h2>
 
 const App = () => {
     return (
-        <div>
+        <div className="container">
             <BrowserRouter>
                 <div>
                     <Header />
